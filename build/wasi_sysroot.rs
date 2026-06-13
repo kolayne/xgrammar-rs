55bc839e13c6c59213ee1c47cead46a77d83d0cc
Nikolai Nechaev <Nikolay_Nechaev@mail.ru>
2026-06-13 13:19:45 +0900

    Universal compiler fix

--- build/wasi_sysroot.rs
+++ build/wasi_sysroot.rs
@@ -24,11 +24,17 @@ pub fn find_or_build_and_set() {
 static NEED_CLANG_LIKE_COMPILER_ERR_MSG: &str =
     "Wasi-sdk requires clang-like C and C++ compilers, but:";
 
-fn wasi_c_compiler() -> Option<cc::Build> {
+fn wasi_c_compiler_cfg() -> Option<cc::Build> {
     let mut c_build_config = cc::Build::new();
     c_build_config.cpp(false);
     let default_c_compiler = c_build_config.get_compiler();
-    if !default_c_compiler.is_like_clang() {
+    if default_c_compiler.is_like_clang() {
+        // Need to set the compiler to itself 🤪
+        // That's because we want to fix this compiler (which we know is clang-like).
+        // Otherwise `cc` may decide to change it later
+        c_build_config.compiler(c_build_config.get_compiler().path());
+    } else {
+        println!("cargo::warning=C: not like clang");
         println!("cargo::warning={NEED_CLANG_LIKE_COMPILER_ERR_MSG}");
         println!(
             "cargo::warning=C compiler at {:?} is not clang-like (you may set the `CC` environment variable to the path to your C compiler)",
@@ -44,11 +50,16 @@ fn wasi_c_compiler() -> Option<cc::Build> {
     Some(c_build_config)
 }
 
-fn wasi_cpp_compiler() -> Option<cc::Build> {
+fn wasi_cpp_compiler_cfg() -> Option<cc::Build> {
     let mut cpp_build_config = cc::Build::new();
     cpp_build_config.cpp(true);
     let default_cpp_compiler = cpp_build_config.get_compiler();
-    if !default_cpp_compiler.is_like_clang() {
+    if default_cpp_compiler.is_like_clang() {
+        // Need to set the compiler to itself 🤪
+        // That's because we want to fix this compiler (which we know is clang-like).
+        // Otherwise `cc` may decide to change it later
+        cpp_build_config.compiler(cpp_build_config.get_compiler().path());
+    } else {
         println!("cargo::warning={NEED_CLANG_LIKE_COMPILER_ERR_MSG}");
         println!(
             "cargo::warning=C++ compiler {:?} is not clang-like (you may set the `CXX` environment variable to the path to your C++ compiler)",
@@ -68,8 +79,8 @@ fn build_wasi_sdk(wasi_sdk_path: &dyn AsRef<Path>) -> PathBuf {
     let wasi_sdk_path = wasi_sdk_path.as_ref();
 
     // Call the two functions together, so that all of their warnings/errors are emitted, even if one of them fails.
-    let (Some(c_compiler), Some(cpp_compiler)) =
-        (wasi_c_compiler(), wasi_cpp_compiler())
+    let (Some(c_compiler_cfg), Some(cpp_compiler_cfg)) =
+        (wasi_c_compiler_cfg(), wasi_cpp_compiler_cfg())
     else {
         // Error messages were printed by `wasi_{c,cpp}_compiler`
         std::process::exit(1);
@@ -82,8 +93,8 @@ fn build_wasi_sdk(wasi_sdk_path: &dyn AsRef<Path>) -> PathBuf {
     cmake_config.out_dir(wasi_build_dir)
         .target(&env::var("HOST").expect("cargo shall set HOST"))
         .no_default_flags(true) // We are in a cross-compiling environment but this build is for the host
-        .init_c_cfg(c_compiler)
-        .init_cxx_cfg(cpp_compiler)
+        .init_c_cfg(c_compiler_cfg)
+        .init_cxx_cfg(cpp_compiler_cfg)
         .define("WASI_SDK_EXCEPTIONS", "ON")
         .define("WASI_SDK_INCLUDE_TESTS", "OFF")
         .profile(match env::var("PROFILE").expect("PROFILE is not set").as_str() {
